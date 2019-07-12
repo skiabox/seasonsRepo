1f6f68f03997e2a25d89d4a494c093f176e0ca8c
Stavros Kefaleas <stavros.kefaleas@gmail.com>
2019-07-12 13:16:57 +0300

    Commit9

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,16 @@
 import React from "react";
 
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach!",
+    iconName: "sun"
+  },
+  winter: {
+    text: "Burr it is cold!",
+    iconName: "snowflake"
+  }
+};
+
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
     return lat > 0 ? "summer" : "winter"; //using ternary expression
@@ -12,14 +23,18 @@ const SeasonDisplay = props => {
   //console.log(props.lat);
 
   const season = getSeason(props.lat, new Date().getMonth());
-  const text = season === "winter" ? "Burr, it is chilly" : "Lets hit the beach";
-  const icon = season === "winter" ? "snowflake" : "sun";
+
+  //const text = season === "winter" ? "Burr, it is chilly" : "Lets hit the beach";
+  //const icon = season === "winter" ? "snowflake" : "sun";
+
+  //destrucure out text and iconName - object destructuring
+  const { text, iconName } = seasonConfig[season];
 
   return (
     <div>
-      <i className={`${icon} icon`} />
+      <i className={`${iconName} icon`} />
       <h1>{text}</h1>
-      <i className={`${icon} icon`} />
+      <i className={`${iconName} icon`} />
     </div>
   );
 };
