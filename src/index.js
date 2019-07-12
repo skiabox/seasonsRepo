6d048349af9f878805b8dd1a4577e54ea1d8cbbc
Stavros Kefaleas <stavros.kefaleas@gmail.com>
2019-07-12 15:04:47 +0300

    Commit11

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 /* const App = () => {
 
@@ -60,7 +61,7 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />;
     }
 
-    return <div>Loading!</div>;
+    return <Spinner />;
   }
 }
 
